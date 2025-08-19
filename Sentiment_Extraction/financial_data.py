7c4be5448e18bf79dc352211d4b9d7659e1ff4cc
Sudhanva <147122943+Sudhanva420@users.noreply.github.com>
2025-08-19 21:01:50 +0530

    Update financial_data.py

--- Sentiment_Extraction/financial_data.py
+++ Sentiment_Extraction/financial_data.py
@@ -1,12 +1,11 @@
-# financial_data.py
 import yfinance as yf
 import pandas as pd
 
+#downloads daily stock data and adds a 'quarter' column
 def fetch_daily_financial_data(ticker, start_date, end_date):
-    """
-    Downloads daily stock data and adds an 'quarter' column (Indian fiscal Q-MAR).
-    """
+
     df = yf.download(ticker, start=start_date, end=end_date, auto_adjust=False)
     df = df.reset_index()
     df['quarter'] = pd.PeriodIndex(df['Date'], freq='Q-MAR').astype(str)
     return df
+
