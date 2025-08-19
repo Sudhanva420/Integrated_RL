f3f20b3086ec5e2fb4c958b0862d86762420eea6
Sudhanva <147122943+Sudhanva420@users.noreply.github.com>
2025-08-19 21:00:08 +0530

    Update data_merging.py

--- Sentiment_Extraction/data_merging.py
+++ Sentiment_Extraction/data_merging.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
+#This function adds a 'report_sentiment' column to daily_df after mapping its 'quarter' to quarterly_map
+
 def merge_report_sentiment(daily_df, quarterly_map):
-    """
-    Adds a 'report_sentiment' column to daily_df by mapping its 'quarter' to quarterly_map.
-    """
     daily_df['report_sentiment'] = daily_df['quarter'].map(quarterly_map)
     return daily_df
 
 def save_dataframe_to_csv(df, path):
     df.to_csv(path, index=False)
+
 
