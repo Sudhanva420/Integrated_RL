e791e8fc099a545ac3b363241e625dd3af8251fe
V G Tharunmadhavan <vg.tharunmadhavan@gmail.com>
2025-08-24 12:21:04 +0530

    Update financial_data.py

--- Sentiment_Extraction/financial_data.py
+++ Sentiment_Extraction/financial_data.py
@@ -1,11 +1,12 @@
 import yfinance as yf
 import pandas as pd
 
-#downloads daily stock data and adds a 'quarter' column
+#Collects daily stock data from yfinance API and adds a 'quarter' column in order to make it easier for merging with quarterly sentiment
 def fetch_daily_financial_data(ticker, start_date, end_date):
 
     df = yf.download(ticker, start=start_date, end=end_date, auto_adjust=False)
     df = df.reset_index()
     df['quarter'] = pd.PeriodIndex(df['Date'], freq='Q-MAR').astype(str)
     return df
+
 
